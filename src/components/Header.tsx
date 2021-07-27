3946e01d7cf75988d1d397b5dca88d63bf709657
takaba <r_takaba@satt.jp>
2021-07-28 00:17:42 +0900

    tailwindCSS

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+// Header.tsx
+
+
+const Header = () => {
+
+return(
+    <div className="headerMenu">
+      <div className="hamburgerMenu">
+        <nav>
+          menu
+        </nav>
+      </div>
+    </div>
+
+  )};
+
+export default Header;
