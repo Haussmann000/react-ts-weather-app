a1c157d875040ef9624cdb4e8b65869283f88350
r_takaba <r_takaba@satt.jp>
2021-08-10 18:09:52 +0900

    ヘッダのレスポンシブ設定を変更

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,7 +14,7 @@ return(
       <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
     </button>
   </div>
-  <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+  <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto sm:hidden">
     <div className="text-sm lg:flex-grow">
       <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
         Docs
@@ -27,7 +27,6 @@ return(
       </a>
     </div>
     <div>
-      <a href="#" className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Download</a>
     </div>
   </div>
 </nav>
